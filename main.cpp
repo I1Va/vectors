2921704e6553e13a5b40b5e420e503fd483fd8d3
Abrutin Ivan Dmitrievich <mai.abrutinid@gmail.com>
2025-09-04 19:29:21 +0300

    add axes building

--- main.cpp
+++ main.cpp
@@ -2,54 +2,102 @@
 #include <QWidget>
 #include <QPainter>
 
-class Point {
+
+class PixelPoint {
     public:
         int x = 0, y = 0;
+
+        PixelPoint(int x, int y) {
+            this->x = x;
+            this->y = y;
+        }
+
+        bool operator<(const PixelPoint Other) { return x < Other.x && y < Other.y; }
+        PixelPoint operator+(const PixelPoint Other) { return PixelPoint(x + Other.x, y + Other.y); } 
 };
 
-class CordSystem : public QWidget {
+class Vec2 {
 
-private:
-    int     cord_scale      = 0;
-    Point   cord_center     = {0, 0};
+    public:
+        double x = 0, y = 0;
 
-    Point   canvas_left_corner  = {0, 0};
-    Point   canvas_right_corner = {0, 0};
-    int     canvas_width        = 0;
-    int     canvas_height       = 0;
-    
-    
+        Vec2(double x, double y) {
+            this->x = x;
+            this->y = y;
+        }
+
+        bool operator<(const Vec2 Other) { return x < Other.x && y < Other.y; }
+        Vec2 operator+(const Vec2 Other) { return Vec2(x + Other.x, y + Other.y); }
+
+};
+
+class Plot : public QWidget {
 
 public:
-    CordSystem(QWidget *parent, Point left_corner, Point right_corner, Point cord_center, int cord_scale)
+    Plot(QWidget *parent, PixelPoint LeftCorner, PixelPoint RightCorner, PixelPoint CordCenter, int CordScale)
         : QWidget(parent) {
-        
-        this->cord_scale            = cord_scale;
-        this->cord_center           = cord_center;
-        
-        this->canvas_left_corner    = left_corner;
-        this->canvas_right_corner   = right_corner;
-        
-        this->canvas_width = right_corner.x - left_corner.x;
-        this->canvas_height = right_corner.y - left_corner.y;
 
+        this->CordScale            = CordScale;
+        this->CordCenter           = CordCenter;
+        
+        this->CanvasLeftCorner    = LeftCorner;
+        this->CanvasRightCorner   = RightCorner;
+        
+        this->CanvasWidth = RightCorner.x - LeftCorner.x;
+        this->CanvasHeight = RightCorner.y - LeftCorner.y;
     }
 
-protected:
+
+
+private:
+    int CordScale = 0;
+    PixelPoint CordCenter = {0, 0};
+
+    PixelPoint CanvasLeftCorner = {0, 0};
+    PixelPoint CanvasRightCorner = {0, 0};
+    int CanvasWidth = 0;
+    int CanvasHeight = 0;
+
+    void MakeAxes() {
+        QPainter painter(this);
+
+        QPen pen(Qt::black);    
+        painter.setPen(pen);
+
+        pen.setWidth(2);
+        painter.drawLine(0, CordCenter.y, CanvasWidth, CordCenter.y);
+        painter.drawLine(CordCenter.x, 0, CordCenter.x, CanvasHeight);
+
+        pen.setWidth(1);
+
+        // Horizontal axe
+        for (int hor_tick_x = CordCenter.x; hor_tick_x < CanvasWidth; hor_tick_x += CordScale) {
+            painter.drawLine(hor_tick_x, CordCenter.y - 1, hor_tick_x, CordCenter.y + 1);
+        }
+        for (int hor_tick_x = CordCenter.x; hor_tick_x >= 0; hor_tick_x -= CordScale) {
+            painter.drawLine(hor_tick_x, CordCenter.y - 1, hor_tick_x, CordCenter.y + 1);
+        }
+
+        // Vertical Axe
+        for (int ver_tick_y = CordCenter.y; ver_tick_y < CanvasHeight; ver_tick_y += CordScale) {
+            painter.drawLine(CordCenter.x - 1, ver_tick_y, CordCenter.x + 1, ver_tick_y);
+        }
+        for (int ver_tick_y = CordCenter.y; ver_tick_y >= 0; ver_tick_y -= CordScale) {
+            painter.drawLine(CordCenter.x - 1, ver_tick_y, CordCenter.x + 1, ver_tick_y);
+        }
+    }
+
     void paintEvent(QPaintEvent *) override {
-        setGeometry(canvas_left_corner.x, canvas_left_corner.y, canvas_width, canvas_height);  
+        setGeometry(CanvasLeftCorner.x, CanvasLeftCorner.y, CanvasWidth, CanvasHeight);  
         setStyleSheet("background-color: white;");
 
-        QPainter painter(this);
-        QPen pen(Qt::black);
-        pen.setWidth(2);    
-
-        painter.setPen(pen); 
-
-        painter.drawLine(0, cord_center.y, canvas_width, cord_center.y);
-        painter.drawLine(cord_center.x, 0, cord_center.x, canvas_height);
+        MakeAxes();
     }
 };
+
+// class CordSystem : public Plot {
+
+// }
 
 int main(int argc, char **argv) {
     QApplication app(argc, argv);
@@ -59,8 +107,8 @@ int main(int argc, char **argv) {
     window.setWindowTitle("Two Canvases");
     window.resize(800, 600);      
 
-    CordSystem *big_plot = new CordSystem(&window, {50, 50}, {550, 550}, {50, 50}, 1);
-    CordSystem *small_plot = new CordSystem(&window, {600, 400}, {750, 550}, {50, 50}, 1);
+    Plot *big_plot = new Plot(&window, {50, 50}, {550, 550}, {50, 50}, 10);
+    Plot *small_plot = new Plot(&window, {600, 400}, {750, 550}, {50, 50}, 10);
 
     window.show();                
 
