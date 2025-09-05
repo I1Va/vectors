aa6802532d24e1b137cb0acd7f27e4409a6ed549
Abrutin Ivan Dmitrievich <mai.abrutinid@gmail.com>
2025-09-05 14:14:03 +0300

    canvas fix

--- inc/visual.h
+++ inc/visual.h
@@ -14,47 +14,14 @@ class PixelPoint {
 
 
 class PlotWidget : public QWidget {
-    Q_OBJECT
 public:
-        PlotWidget(QWidget *parent, PixelPoint LeftCorner, PixelPoint RightCorner, PixelPoint CordCenter, int CordScale);
+        explicit PlotWidget(QWidget *parent, PixelPoint LeftCorner, PixelPoint RightCorner, PixelPoint CordCenter, int CordScale);
         void DrawVector(QPainter *painter, PixelPoint VecStart, PixelPoint VecEnd, double ArrowScale, double ArrowAngle); 
     
     
 protected:
+    void paintEvent(QPaintEvent *event) override;
 
-    void paintEvent(QPaintEvent *) override {
-        QPainter painter(this);
-
-    setGeometry(CanvasLeftCorner.x, CanvasLeftCorner.y, CanvasWidth, CanvasHeight);  
-    setStyleSheet("background-color: white;");
-
-    // MakeAxes(&painter);
-
-    QPen pen(Qt::black);    
-    painter.setPen(pen);
-
-    pen.setWidth(2);
-    painter.drawLine(0, CordCenter.y, CanvasWidth, CordCenter.y);
-    painter.drawLine(CordCenter.x, 0, CordCenter.x, CanvasHeight);
-
-    pen.setWidth(1);
-    
-    // Horizontal axe
-    for (int hor_tick_x = CordCenter.x; hor_tick_x < CanvasWidth; hor_tick_x += CordScale) {
-        painter.drawLine(hor_tick_x, CordCenter.y - 1, hor_tick_x, CordCenter.y + 1);
-    }
-    for (int hor_tick_x = CordCenter.x; hor_tick_x >= 0; hor_tick_x -= CordScale) {
-        painter.drawLine(hor_tick_x, CordCenter.y - 1, hor_tick_x, CordCenter.y + 1);
-    }
-
-    // Vertical Axe
-    for (int ver_tick_y = CordCenter.y; ver_tick_y < CanvasHeight; ver_tick_y += CordScale) {
-        painter.drawLine(CordCenter.x - 1, ver_tick_y, CordCenter.x + 1, ver_tick_y);
-    }
-    for (int ver_tick_y = CordCenter.y; ver_tick_y >= 0; ver_tick_y -= CordScale) {
-        painter.drawLine(CordCenter.x - 1, ver_tick_y, CordCenter.x + 1, ver_tick_y);
-    }
-        }
 
 private:
     void MakeAxes(QPainter *painter);
--- src/visual.cpp
+++ src/visual.cpp
@@ -3,6 +3,7 @@
 #include <QPainter>
 #include <QMutex>
 
+#include <iostream>
 #include "geometry.h"
 #include "visual.h"
 
@@ -28,7 +29,11 @@ PlotWidget::PlotWidget(QWidget *parent,
     , CordCenter(CordCenter)
     , CordScale(CordScale)
 {
-    setAttribute(Qt::WA_OpaquePaintEvent);  
+    CanvasWidth = CanvasRightCorner.x - CanvasLeftCorner.x;
+    CanvasHeight = CanvasRightCorner.y - CanvasLeftCorner.y;
+
+    setGeometry(CanvasLeftCorner.x, CanvasLeftCorner.y, CanvasWidth, CanvasHeight);
+    setStyleSheet("background-color: white;");
 }   
 
 void PlotWidget::DrawVector(QPainter *painter, PixelPoint VecStart, PixelPoint VecEnd, double ArrowScale=DEFAULT_ARROW_SCALE, double ArrowAngle=DEFAULT_ARROW_ANGLE) {
@@ -77,36 +82,8 @@ void PlotWidget::MakeAxes(QPainter *painter) {
 
 }
 
-// void PlotWidget::paintEvent(QPaintEvent *) {
-//     QPainter painter(this);
+void PlotWidget::paintEvent(QPaintEvent *event) {
+    QPainter painter(this);
 
-//     setGeometry(CanvasLeftCorner.x, CanvasLeftCorner.y, CanvasWidth, CanvasHeight);  
-//     setStyleSheet("background-color: white;");
-
-//     // MakeAxes(&painter);
-
-//     QPen pen(Qt::black);    
-//     painter.setPen(pen);
-
-//     pen.setWidth(2);
-//     painter.drawLine(0, CordCenter.y, CanvasWidth, CordCenter.y);
-//     painter.drawLine(CordCenter.x, 0, CordCenter.x, CanvasHeight);
-
-//     pen.setWidth(1);
-    
-//     // Horizontal axe
-//     for (int hor_tick_x = CordCenter.x; hor_tick_x < CanvasWidth; hor_tick_x += CordScale) {
-//         painter.drawLine(hor_tick_x, CordCenter.y - 1, hor_tick_x, CordCenter.y + 1);
-//     }
-//     for (int hor_tick_x = CordCenter.x; hor_tick_x >= 0; hor_tick_x -= CordScale) {
-//         painter.drawLine(hor_tick_x, CordCenter.y - 1, hor_tick_x, CordCenter.y + 1);
-//     }
-
-//     // Vertical Axe
-//     for (int ver_tick_y = CordCenter.y; ver_tick_y < CanvasHeight; ver_tick_y += CordScale) {
-//         painter.drawLine(CordCenter.x - 1, ver_tick_y, CordCenter.x + 1, ver_tick_y);
-//     }
-//     for (int ver_tick_y = CordCenter.y; ver_tick_y >= 0; ver_tick_y -= CordScale) {
-//         painter.drawLine(CordCenter.x - 1, ver_tick_y, CordCenter.x + 1, ver_tick_y);
-//     }
-// }
+    MakeAxes(&painter);
+}
